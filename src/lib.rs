5d7bc294975bd684902b8c9d6fc8c770f716d155
Ante Perisic <perisic.ante@google.com>
2024-09-04 08:43:51 +0200

    add xor test

--- src/lib.rs
+++ src/lib.rs
@@ -72,6 +72,44 @@ pub struct OrGate {
     input_pins: Vec<Pin>,
     output: Pin,
 }
+pub struct XOrGate {
+    pub no_of_inputs: u8,
+    pub input_pins: Vec<Pin>,
+    pub output: Pin,
+}
+
+impl XOrGate {
+    pub fn new(no_of_input: u8) -> Self {
+        let mut pin_vec: Vec<Pin> = Vec::new();
+        for _ in 0..no_of_input {
+            pin_vec.push(Pin::new_disconected());
+        }
+        XOrGate {
+            no_of_inputs: no_of_input,
+            input_pins: pin_vec,
+            output: Pin::new_disconected(),
+        }
+    }
+}
+
+
+impl OutputResult for XOrGate {
+    fn calculate(self) -> Output {
+        let init_val  = self.input_pins.get(0).unwrap().get_value();
+        let result =
+            self.input_pins.iter().skip(1).fold( //skip first element because its value is init value of acc
+                init_val,
+                |acc, num| acc ^ num.get_value(),
+            );
+        let mut output_pin = Pin::new_disconected();
+        if result == 1 {
+            output_pin.set_high();
+        } else {
+            output_pin.set_low();
+        }
+        Output(output_pin)
+    }
+}
 
 pub struct AndGate {
     pub no_of_inputs: u8,
@@ -98,7 +136,7 @@ impl OutputResult for AndGate {
         let result =
             self.input_pins.iter().fold(
                 1,
-                |acc, num| if (acc & num.get_value()) == 1 { 1 } else { 0 },
+                |acc, num| acc & num.get_value(),
             );
         let mut output_pin = Pin::new_disconected();
         if result == 1 {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use gates::{AndGate,OutputResult};
+use gates::{AndGate,OutputResult, XOrGate};
 
 fn main() {}
 
 #[test]
-fn and_output() {
+fn and_gate_output() {
     let mut gate1 = AndGate::new(3);
     gate1.input_pins.get_mut(0).unwrap().set_low();
     gate1.input_pins.get_mut(1).unwrap().set_high();
@@ -13,11 +13,28 @@ fn and_output() {
 }
 
 #[test]
-fn and_output2() {
+fn and_gate_output2() {
     let mut gate2 = AndGate::new(3);
     gate2.input_pins.get_mut(0).unwrap().set_high();
     gate2.input_pins.get_mut(1).unwrap().set_high();
     gate2.input_pins.get_mut(2).unwrap().set_high();
     let gate2_output = gate2.calculate();
     assert_eq!(1, gate2_output.get_u8_result());
+}
+
+#[test]
+fn xor_gate_output() {
+    let mut xorgate = XOrGate::new(3);
+    xorgate.input_pins.get_mut(0).unwrap().set_high();
+    xorgate.input_pins.get_mut(1).unwrap().set_low();
+    xorgate.input_pins.get_mut(2).unwrap().set_high();
+    assert_eq!(0,xorgate.calculate().get_u8_result());
+}
+#[test]
+fn xor_gate_output2() {
+    let mut xorgate = XOrGate::new(3);
+    xorgate.input_pins.get_mut(0).unwrap().set_low();
+    xorgate.input_pins.get_mut(1).unwrap().set_high();
+    xorgate.input_pins.get_mut(2).unwrap().set_low();
+    assert_eq!(1,xorgate.calculate().get_u8_result());
 }
